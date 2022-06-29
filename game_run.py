b856120921084055f2a35f3ab17ac3011352cba4
Crony Akatsuki <crolinuxgamer@gmail.com>
2022-06-29 15:49:31 +0200

    Revamp, removing advanced launching from cmd, only lauching games from config for now.

--- game_run.py
+++ game_run.py
@@ -6,7 +6,7 @@ import subprocess
 import configparser
 import pathlib
 
-VERSION = "1.4"
+VERSION = "2"
 
 
 def set_vsync(cmd, mode):
@@ -129,74 +129,6 @@ def list_by_category(list, config):
     sys.exit(0)
 
 
-def cmd_gen_args(args, cmd):
-    if args.fps_limit and args.vsync:
-        cmd.append('strangle')
-        cmd = set_fps(cmd, str(args.fps_limit))
-        cmd = set_vsync(cmd, args.vsync)
-    elif args.fps_limit:
-        cmd.append('strangle')
-        cmd = set_fps(cmd, str(args.fps_limit))
-    elif args.vsync:
-        cmd.append('strangle')
-        cmd = set_vsync(cmd, args.vsync)
-
-    if args.gamemode:
-        cmd.append('gamemoderun')
-
-    if args.mangohud:
-        cmd = enable_mangohud(cmd, args.dlsym)
-
-    if args.prefix and not args.wine:
-        print('Wine needs to be provided with prefix!')
-        sys.exit(1)
-
-    if args.wine and not args.prefix:
-        print('Prefix must also be provided!')
-        sys.exit(1)
-    elif args.wine:
-        cmd = set_wine(cmd, args.prefix, args.wine)
-
-    if args.sync and not args.wine:
-        print('Sync option needs for wine to be set!')
-        sys.exit(1)
-    elif args.sync:
-        set_wine_sync(args.sync)
-
-    if args.fsr and not args.wine:
-        print('Fsr option needs for wine to be set!')
-        sys.exit(1)
-    elif args.fsr:
-        set_fsr()
-
-    if args.dxvk and not args.wine:
-        print('Dxvk option needs for wine to be set!')
-        sys.exit(1)
-    elif args.dxvk:
-        set_dxvk_optimizations(args.prefix)
-
-    if args.prime:
-        set_nvidia_prime()
-
-    if args.java:
-        cmd = set_java(cmd)
-
-    if args.flatpak:
-        cmd = set_flatpak(cmd)
-
-    if args.game:
-        cmd.append(args.game)
-
-    if args.args:
-        for arg in args.args.split(' '):
-            cmd.append(arg)
-
-    if args.working_dir:
-        os.chdir(args.working_dir)
-
-    return cmd
-
-
 def cmd_gen_config(config, cmd, game):
     if config.has_option(game, 'fps') and config.has_option(game, 'vsync'):
         cmd.append('strangle')
@@ -287,58 +219,32 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-v', '--version', action='store_true',
                         help='Print program version')
-    parser.add_argument('game', type=str, metavar='GAME', action='store', nargs='?',
-                        help="Command or path to the game you wanna run")
-    parser.add_argument('--list', type=str, metavar='ITEM', action='store',
-                        help='List games that you sorted by category or categories themself')
-    parser.add_argument('--java', action='store_true',
-                        help='Run a java game')
-    parser.add_argument('--flatpak', action='store_true',
-                        help='Run a flatpak game')
-    parser.add_argument('-a', '--args', type=str, metavar='ARGUMENTS', action='store',
-                        help='Additional game arguments')
-    parser.add_argument('-g', '--gamemode', action='store_true',
-                        help='Run game with gamemode')
-    parser.add_argument('-m', '--mangohud', action='store_true',
-                        help='Run game with mangohud')
-    parser.add_argument('--dlsym', action='store_true',
-                        help='Enable dlsym loading for mangohud')
-    parser.add_argument('-f', '--fps_limit', type=int, metavar='FPS', action='store',
-                        help="Limit game fps (Must have libstrangle installed)")
-    parser.add_argument('--vsync', type=str, metavar='VSYNC', action='store',
-                        help="Set vsync on for either opengl or vulkan (Must have libstrangle installed)")
-    parser.add_argument('-w', '--wine', type=str, metavar='WINE', action='store',
-                        help='Specify either WINE or PROTON version you wanna use(NEEDS FOR PREFIX TO BE SET)')
-    parser.add_argument('-p', '--prefix', type=str, metavar='WINE_PREFIX', action='store',
-                        help='Path to wine prefix')
-    parser.add_argument('-s', '--sync', type=str, metavar='WINE_SYNC', action='store',
-                        help='Specify what sync method to use with WINE')
-    parser.add_argument('--fsr', action='store_true',
-                        help='Enable fsr for any wine game')
-    parser.add_argument('-d', '--dxvk', action='store_true',
-                        help='Enable specific dxvk settings')
-    parser.add_argument('--prime', action='store_true',
-                        help='Use dedicated gpu on an optimus system')
-    parser.add_argument('--working_dir', type=str, metavar='WORKING_DIRECTORY', action='store',
-                        help='Set the working directory for games that need it')
+
+    subparser = parser.add_subparsers(dest='command')
+
+    list = subparser.add_parser(
+        'list', help="List game category's or games from a category")
+    list.add_argument('category', type=str, metavar='CATEGORY', action='store', nargs='?',
+                      help="Category to print games from")
+
+    launch = subparser.add_parser('launch', help="Launch specified game")
+    launch.add_argument('game', type=str, metavar='GAME', action='store', nargs='?',
+                        help="Game name from the config")
+
+    add = subparser.add_parser('launch', help="Add a game to the config")
+    add.add_argument('name', type=str, metavar='NAME', action='store', nargs='?',
+                        help="Name of the game to add")
 
     config = configparser.ConfigParser()
 
-    if os.path.exists(os.path.expanduser("~/.config/game-run")) and os.path.isfile(os.path.expanduser("~/.config/game-run/config.ini")):
+    if os.path.isfile(os.path.expanduser("~/.config/game-run/config.ini")):
         configExists = True
         config.read(os.path.expanduser("~/.config/game-run/config.ini"))
     else:
         configExists = False
 
-    if len(sys.argv) == 1 and not configExists:
+    if len(sys.argv) == 1:
         parser.print_help()
-        sys.exit(1)
-    elif len(sys.argv) == 1 and len(config.sections()) == 0:
-        parser.print_help()
-        sys.exit(1)
-    elif len(sys.argv) == 1 and not len(config.sections()) == 0:
-        for game in config.sections():
-            print(game)
         sys.exit(0)
 
     args = parser.parse_args()
@@ -349,20 +255,23 @@ def main():
         print(f'Version: {VERSION}')
         sys.exit(0)
 
-    if args.list and configExists:
-        list_by_category(args.list, config)
-    elif args.list and not configExists:
-        print('This options requires for you to make a games config')
+    if not configExists:
+        print("No config existing, add games first")
         sys.exit(1)
 
-    if not args.game:
-        print('Game must be provided')
-        sys.exit(1)
+    if args.command == 'list':
+        if args.category:
+            list_by_category(args.category, config)
+        else:
+            list_categories(config)
+        sys.exit(0)
 
-    if len(sys.argv) == 2 and configExists and config.has_section(args.game):
-        cmd = cmd_gen_config(config, cmd, args.game)
-    else:
-        cmd = cmd_gen_args(args, cmd)
+    if args.command == 'launch':
+        if args.game:
+            cmd = cmd_gen_config(config, cmd, args.game)
+        else:
+            print('Game must be provided')
+            sys.exit(1)
 
     subprocess.Popen(cmd)
     sys.exit(0)
